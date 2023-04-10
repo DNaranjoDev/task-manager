7d49413cf2ea0b3f6f44d385bf3f2988e4e8d663
David Naranjo <davidnaranjodev@gmail.com>
2023-04-10 15:52:12 +0100

    Initialize app

---
+++ lib/app/app.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+import 'package:task_manager/app/view/home.dart';
+import 'package:task_manager/app/view/splash.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const primary = Color(0xFF008ae6);
+    const textColor = Color(0xFF4A4A4A);
+    const backgroundColor = Color(0xFFF5F5F5);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: primary),
+        scaffoldBackgroundColor: backgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: 'Hack',
+          bodyColor: textColor,
+          displayColor: textColor,
+        ),
+      ),
+      home: SplashPage(),
+    );
+  }
+}
